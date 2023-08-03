255b8387345e669de46643836ddc4462ba02b5b5
Kristaps Ritins <kritinsdev@gmail.com>
2023-08-03 21:55:39 +0300

    chng

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -81,10 +81,11 @@ class ListingsController extends Controller
         $modelStat = ModelStat::where('model_id', $listing->model_id)->first();
 
         $listing['model'] = $listing->listingModel->model_name;
+        // unset($listing['listing_model']);
 
         if($listing->model_id != 24) {
             if (($modelStat->average_price - $listing->price) >= 70) {
-                Mail::to('krlistingstrackcer@gmail.com')->send(new ListingCreated($listing));
+                // Mail::to('krlistingstrackcer@gmail.com')->send(new ListingCreated($listing));
             }
         }
 
